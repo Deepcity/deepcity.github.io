31cdebf615dcd08fbad92d6e641822809bd8161d
Deepcity <91273161+Deepcity@users.noreply.github.com>
2024-12-20 18:21:35 +0800

    Update RSS

--- Update.bat
+++ Update.bat
@@ -6,4 +6,4 @@ if not defined FNM_AUTORUN_GUARD (
 	set "FNM_AUTORUN_GUARD=AutorunGuard"
 	FOR /f "tokens=*" %%z IN ('fnm env --use-on-cd') DO CALL %%z
 )
-hexo g && hexo d && git add . && git commit -m "常规文章更新" && git push origin main
+hexo g && hexo d && git add . && git commit -m "Update RSS" && git push origin main
